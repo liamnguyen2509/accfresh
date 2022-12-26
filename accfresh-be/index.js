f398c47545365f2700fb0db7aeb51f75cdb0d615
Liam Nguyen <lam.nguyenthai2509@gmail.com>
2022-12-26 18:35:02 +0700

    revert port

--- accfresh-be/index.js
+++ accfresh-be/index.js
@@ -3,7 +3,7 @@ const server = require('./server');
 const port = process.env.PORT || 5000;
 
 const startServer = () => {
-    server.listen(443, () => {
+    server.listen(port, () => {
         console.log(`Server running on port ${port}`);
     });
 };
