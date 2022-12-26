63be24cc2b34ab283e591e8e36e0641d5dc74c55
Liam Nguyen <lam.nguyenthai2509@gmail.com>
2022-12-26 18:24:13 +0700

    change port to 443

--- accfresh-be/index.js
+++ accfresh-be/index.js
@@ -3,7 +3,7 @@ const server = require('./server');
 const port = process.env.PORT || 5000;
 
 const startServer = () => {
-    server.listen(port, () => {
+    server.listen(443, () => {
         console.log(`Server running on port ${port}`);
     });
 };
