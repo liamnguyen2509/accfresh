7859279ff6b85f73e90267311720d2b128ddf3e1
Liam Nguyen <lam.nguyenthai2509@gmail.com>
2023-02-15 13:21:26 +0700

    Thank you page

--- accfresh-fe/src/App.js
+++ accfresh-fe/src/App.js
@@ -10,19 +10,20 @@ import UserDetails from './pages/admin/Users/components/UserDetails';
 
 const App = () => {
   return (
-    <Suspense fallback={<Spinner />}>
-      <Routes>
-        {routes.map(({ component: Component, path, ...rest}) => {
-          return (
-            <Route key={path} path={path} element={<Component />} {...rest} />
-          );
-        })}
-        <Route key="/payments/:paymentId" path="/payments/:paymentId" element={<PaymentForm />} />
-        <Route key="/orders/:orderId" path="/orders/:orderId" element={<OrderDetails />} />
-        <Route key="/admin/accounts/import" path="/admin/accounts/import" element={<ImportForm />} />
-        <Route key="/admin/users/:userId" path="/admin/users/:userId" element={<UserDetails />} />
-      </Routes>  
-    </Suspense>
+    // <Suspense fallback={<Spinner />}>
+    //   <Routes>
+    //     {routes.map(({ component: Component, path, ...rest}) => {
+    //       return (
+    //         <Route key={path} path={path} element={<Component />} {...rest} />
+    //       );
+    //     })}
+    //     <Route key="/payments/:paymentId" path="/payments/:paymentId" element={<PaymentForm />} />
+    //     <Route key="/orders/:orderId" path="/orders/:orderId" element={<OrderDetails />} />
+    //     <Route key="/admin/accounts/import" path="/admin/accounts/import" element={<ImportForm />} />
+    //     <Route key="/admin/users/:userId" path="/admin/users/:userId" element={<UserDetails />} />
+    //   </Routes>  
+    // </Suspense>
+    <h1 style={{textAlign: 'center', verticalAlign: 'middle', marginTop: '20%', color: '#fff'}}>Thank You for used my service</h1>
   );
 }
 
